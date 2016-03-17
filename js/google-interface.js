3d83364ad9ffd21360b9bd5f875b30278179809d
Andrew Telleria and Jake Steinberg <atelleria08@gmail.com>
2016-03-17 14:20:25 -0700

    updates routes to be selectable, and actually work

--- js/google-interface.js
+++ js/google-interface.js
@@ -2,6 +2,11 @@ var map;
 var portland;
 var path;
 var request;
+
+var locationArray = [
+  [45.520786, -122.677733, 114, 13, 1],//epicodus
+  [45.522636, -122.673347, 100, 0, 0] //voodoo
+];
 // var lat;
 // var long;
 function initialize(lat, long, heading, pitch, zoom) {
@@ -13,30 +18,40 @@ function initialize(lat, long, heading, pitch, zoom) {
   portland = {lat: lat, lng: long};
   var destination = {lat: lat, lng: long};
   var epicodus = {lat: 45.520786, lng: -122.677733};
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var directionsService = new google.maps.DirectionsService;
+
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: portland,
-    zoom: 14
-  });
+           zoom: 14,
+           center: epicodus
+         });
+         directionsDisplay.setMap(map);
 
-  var directionsDisplay = new google.maps.DirectionsRenderer({
-    map: map
-  });
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+    document.getElementById('mode').addEventListener('change', function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+ });
 
-  request = {
-  destination: destination,
-  origin: epicodus,
-  travelMode: google.maps.TravelMode.TRANSIT
 
-};
-
-var directionsService = new google.maps.DirectionsService();
-  directionsService.route(request, function(response, status) {
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  var selectedMode = document.getElementById('mode').value;
+  directionsService.route({
+    origin: {lat: 45.520786, lng: -122.677733},  // Haight.
+    destination: {lat: lat, lng: long},  // Ocean Beach.
+    // Note that Javascript allows us to access the constant
+    // using square brackets and a string value as its
+    // "property."
+    travelMode: google.maps.TravelMode[selectedMode]
+  }, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
-      // Display the route on the map.
       directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
     }
   });
+}
+
 
 
 
@@ -66,7 +81,6 @@ var directionsService = new google.maps.DirectionsService();
     map: map
   });
 
-
  animateCircle(line);
 }
 
@@ -79,164 +93,55 @@ function animateCircle(line) {
     icons[0].offset = (count / 2) + '%';
     line.set('icons', icons);
   }, 20);
-
 }
 
 
 
 
 $(document).ready(function() {
+
   initialize(45.520786, -122.677733, 114, 13, 1);
-//vodoo
-$('#voodoo').submit(function(event) {
-  initialize(45.522636, -122.673347, 100, 0, 0);
-  $('#voodooOptions').toggle();
-  event.preventDefault();
-});
 
-$('#transitVoodoo').submit(function(event) {
-  event.preventDefault();
-  initialize(45.522636, -122.673347, 100, 0, 0);
-  request.travelMode = google.maps.TravelMode.TRANSIT
-});
+  //vodoo
+  $('#voodoo').submit(function(event) {
+    initialize(45.522636, -122.673347, 100, 0, 0);
+    $('#voodooOptions').toggle();
+    event.preventDefault();
+  });
 
-$('#walkVoodoo').submit(function(event) {
-  event.preventDefault();
-  initialize(45.522636, -122.673347, 100, 0, 0);
-  request.travelMode = google.maps.TravelMode.WALKING
-});
+  //providence
+  $('#providence').submit(function(event) {
+    initialize(45.522021, -122.690165, 270, 0, 2);
+    $('#providenceOptions').toggle();
+    event.preventDefault();
+  });
 
-$('#driveVoodoo').submit(function(event) {
-  event.preventDefault();
-  initialize(45.522636, -122.673347, 100, 0, 0);
-  request.travelMode = google.maps.TravelMode.DRIVING
-});
+  //10 barel
+  $('#10barrel').submit(function(event) {
+    initialize(45.525777, -122.685391, 330, 0, 1.5);
+    $('#barrelOptions').toggle();
+    event.preventDefault();
+  });
 
+  //hop works
+  $('#hopWorks').submit(function(event) {
+    initialize(45.497276, -122.635482, 130, 0, 3.5);
+    $('#hopOptions').toggle();
+    event.preventDefault();
+  });
 
-//providence
-$('#providence').submit(function(event) {
-initialize(45.522021, -122.690165, 270, 0, 2);
-$('#providenceOptions').toggle();
-event.preventDefault();
-});
+  //mode center
+  $('#modaCenter').submit(function(event) {
+    console.log("moda");
+    initialize(45.530541, -122.666653, 10, 20, 2);
+    $('#modaOptions').toggle();
+    event.preventDefault();
+  });
 
-$('#transitProvidence').submit(function(event) {
-  event.preventDefault();
-  initialize(45.522021, -122.690165, 270, 0, 2);
-  request.travelMode = google.maps.TravelMode.TRANSIT
-});
-
-$('#walkProvidence').submit(function(event) {
-  event.preventDefault();
-  initialize(45.522021, -122.690165, 270, 0, 2);
-  request.travelMode = google.maps.TravelMode.WALKING
-});
-
-$('#driveProvidence').submit(function(event) {
-  event.preventDefault();
-  initialize(45.522021, -122.690165, 270, 0, 2);
-  request.travelMode = google.maps.TravelMode.DRIVING
-});
-
-//10 barel
-$('#10barrel').submit(function(event) {
-initialize(45.525777, -122.685391, 330, 0, 1.5);
-$('#barrelOptions').toggle();
-event.preventDefault();
-});
-
-$('#transitBarrel').submit(function(event) {
-  event.preventDefault();
-  initialize(45.525777, -122.685391, 330, 0, 1.5);
-  request.travelMode = google.maps.TravelMode.TRANSIT
-});
-
-$('#walkBarrel').submit(function(event) {
-  event.preventDefault();
-  initialize(45.525777, -122.685391, 330, 0, 1.5);
-  request.travelMode = google.maps.TravelMode.WALKING
-});
-
-$('#driveBarrel').submit(function(event) {
-  event.preventDefault();
-  initialize(45.525777, -122.685391, 330, 0, 1.5);
-  request.travelMode = google.maps.TravelMode.DRIVING
-});
-
-//hop works
-$('#hopWorks').submit(function(event) {
-initialize(45.497276, -122.635482, 130, 0, 3.5);
-$('#hopOptions').toggle();
-event.preventDefault();
-});
-
-$('#transitHop').submit(function(event) {
-  event.preventDefault();
-  initialize(45.497276, -122.635482, 130, 0, 3.5);
-  request.travelMode = google.maps.TravelMode.TRANSIT
-});
-
-$('#walkHop').submit(function(event) {
-  event.preventDefault();
-  initialize(45.497276, -122.635482, 130, 0, 3.5);
-  request.travelMode = google.maps.TravelMode.WALKING
-});
-
-$('#driveHop').submit(function(event) {
-  event.preventDefault();
-  initialize(45.497276, -122.635482, 130, 0, 3.5);
-  request.travelMode = google.maps.TravelMode.DRIVING
-});
-
-//mode center
-$('#modaCenter').submit(function(event) {
-  console.log("moda");
-initialize(45.530541, -122.666653, 10, 20, 2);
-$('#modaOptions').toggle();
-event.preventDefault();
-});
-
-$('#transitModa').submit(function(event) {
-  event.preventDefault();
-  initialize(45.530541, -122.666653, 10, 20, 2);
-  request.travelMode = google.maps.TravelMode.TRANSIT
-});
-
-$('#walkModa').submit(function(event) {
-  event.preventDefault();
-  initialize(45.530541, -122.666653, 10, 20, 2);
-  request.travelMode = google.maps.TravelMode.WALKING
-});
-
-$('#driveModa').submit(function(event) {
-  event.preventDefault();
-  initialize(45.530541, -122.666653, 10, 20, 2);
-  request.travelMode = google.maps.TravelMode.DRIVING
-});
-
-//epicodus
-$('#epicodus').submit(function(event) {
-  initialize(45.520786, -122.677733, 114, 13, 1);
-  $('#epiOptions').toggle();
-event.preventDefault();
-});
-
-$('#transitEpi').submit(function(event) {
-  event.preventDefault();
-  initialize(45.520786, -122.677733, 114, 13, 1);
-  request.travelMode = google.maps.TravelMode.TRANSIT
-});
-
-$('#walkEpi').submit(function(event) {
-  event.preventDefault();
-  initialize(45.520786, -122.677733, 114, 13, 1);
-  request.travelMode = google.maps.TravelMode.WALKING
-});
-
-$('#driveEpi').submit(function(event) {
-  event.preventDefault();
-  initialize(45.520786, -122.677733, 114, 13, 1);
-  request.travelMode = google.maps.TravelMode.DRIVING
-});
-
+  //epicodus
+  $('#epicodus').submit(function(event) {
+    initialize(45.520786, -122.677733, 114, 13, 1);
+    $('#epiOptions').toggle();
+    event.preventDefault();
+  });
 });
